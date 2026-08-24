bbbfae75bb4fcc88f21910c2e451ce8f38e76830
Daniel Noble <danieln539@gmail.com>
2026-08-24 22:37:55 +0200

    rename

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,13 +7,13 @@ import java.util.Map;
 public class Application {
 
     public static void main(String[] args) {
-        MagicEvent premierDraft = new LimitedChampionShip(new Probability(BigDecimal.valueOf(0.6)));
+        MagicEvent limitedChampionShip = new LimitedChampionShip(new Probability(BigDecimal.valueOf(0.6)));
         String gewinnwahrscheinlichkeit = "0.55";
         Probability gameWinPercentage = new Probability(new BigDecimal(gewinnwahrscheinlichkeit));
-        Map<Integer, BigDecimal> winToPercentageMap = premierDraft.calculateLikeliHoodPerWinForGivenGameWinPercentage(gameWinPercentage);
+        Map<Integer, BigDecimal> winToPercentageMap = limitedChampionShip.calculateLikeliHoodPerWinForGivenGameWinPercentage(gameWinPercentage);
         winToPercentageMap.forEach(
                 (integer, percentage) -> System.out.println("Bei einem Premierdraft mit Siegwahrscheinlichkeit pro Spiel von " + gewinnwahrscheinlichkeit + " beträgt die Wahrscheinlichkeit für " + integer + " Siege " + percentage)
         );
-        System.out.println("Der Gewinn beträgt bei einer Wahrscheinlichkeit von " + gewinnwahrscheinlichkeit + " " + premierDraft.calculateWinningForGivenPercentage(gameWinPercentage).amount());
+        System.out.println("Der Gewinn beträgt bei einer Wahrscheinlichkeit von " + gewinnwahrscheinlichkeit + " " + limitedChampionShip.calculateWinningForGivenPercentage(gameWinPercentage).amount());
     }
 }
