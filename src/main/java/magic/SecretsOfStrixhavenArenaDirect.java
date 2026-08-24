ecbf38036f874327eddc78bcc0a510a176d1a5ca
Daniel Noble <danieln539@gmail.com>
2026-08-24 22:35:52 +0200

    add arena direct

---
+++ src/main/java/magic/SecretsOfStrixhavenArenaDirect.java
@@ -0,0 +1,24 @@
+package magic;
+
+import finance.CashAmount;
+import finance.Currency;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Set;
+
+public class SecretsOfStrixhavenArenaDirect extends MagicEvent {
+    public SecretsOfStrixhavenArenaDirect() {
+        super(7, 2, new CashAmount(BigDecimal.valueOf(6000), Currency.GEMS), Map.of(
+                0, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(0L), Currency.GEMS))),
+                1, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(0L), Currency.GEMS))),
+                2, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(0L), Currency.GEMS))),
+                3, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(2700L), Currency.GEMS))),
+                4, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(5400L), Currency.GEMS))),
+                5, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(8100L), Currency.GEMS))),
+                6, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(10800L), Currency.GEMS))),
+                7, Set.of(new CashPrice(new CashAmount(BigDecimal.valueOf(320), Currency.EURO)))
+        ));
+    }
+
+}
